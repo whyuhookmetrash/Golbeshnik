8613a0786be2bcbaade44ed00a39e3926780a290
Vladislav Melnikov <melnikoff2222@gmail.com>
2024-11-10 18:24:12 +0300

    updated movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,67 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5f;
-    public float rotationSpeed = 100f;
-    public float acceleration = 2f;
-    public float mouseSensitivity = 1f;
-    public float verticalRotationSpeed = 100f;
+    
 
-    private float currentSpeed;
-    private Transform cameraTransform;
-    private float verticalRotation;
-    private Vector2 lastMousePosition; // Хранит предыдущее положение мыши
+        public float walkingSpeed = 7.5f;
+        public float runningSpeed = 11.5f;
+        public float jumpSpeed = 8.0f;
+        public float gravity = 20.0f;
+        public Camera playerCamera;
+        public float lookSpeed = 2.0f;
+        public float lookXLimit = 45.0f;
 
-    void Start()
-    {
-        cameraTransform = Camera.main.transform;
-        lastMousePosition = Input.mousePosition;
-    }
+        CharacterController characterController;
+        Vector3 moveDirection = Vector3.zero;
+        float rotationX = 0;
 
-    void Update()
-    {
-        // Получаем ввод с клавиатуры
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        [HideInInspector]
+        public bool canMove = true;
 
-        // Получаем относительное движение мыши
-        Vector2 mouseDelta = (Vector2)Input.mousePosition - lastMousePosition;
-        lastMousePosition = Input.mousePosition; // Обновляем предыдущее положение
-
-        // Вращение камеры и игрока с помощью мыши
-        float mouseX = mouseDelta.x * mouseSensitivity; // Используем относительное движение
-        float mouseY = mouseDelta.y * mouseSensitivity;
-
-        cameraTransform.Rotate(Vector3.up * mouseX);
-        transform.Rotate(Vector3.up * mouseX);
-
-        // Вращение камеры по вертикали
-        verticalRotation -= mouseY;
-        verticalRotation = Mathf.Clamp(verticalRotation, -80f, 80f);
-        cameraTransform.localEulerAngles = new Vector3(verticalRotation, cameraTransform.localEulerAngles.y, 0);
-
-        // Движение в направлении, заданном камерой
-        Vector3 horizontalDirection = cameraTransform.forward; // Используем только горизонтальное направление
-        horizontalDirection.y = 0; // Обнуляем вертикальную составляющую
-        horizontalDirection.Normalize(); // Нормализуем вектор
-
-        Vector3 movement = horizontalDirection * verticalInput + cameraTransform.right * horizontalInput;
-        transform.Translate(movement * speed * Time.deltaTime);
-
-        // Ускорение с помощью Shift
-        if (Input.GetKey(KeyCode.LeftShift))
+        void Start()
         {
-            currentSpeed += acceleration * Time.deltaTime * 2f;
+            characterController = GetComponent<CharacterController>();
+
+            // Lock cursor
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
-        else
+
+        void Update()
         {
-            currentSpeed += acceleration * Time.deltaTime;
+            // We are grounded, so recalculate move direction based on axes
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            // Press Left Shift to run
+            bool isRunning = Input.GetKey(KeyCode.LeftShift);
+            float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+            float movementDirectionY = moveDirection.y;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            else
+            {
+                moveDirection.y = movementDirectionY;
+            }
+
+            // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+            // as an acceleration (ms^-2)
+            if (!characterController.isGrounded)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+
+            // Move the controller
+            characterController.Move(moveDirection * Time.deltaTime);
+
+            // Player and Camera rotation
+            if (canMove)
+            {
+                rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+                rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+                playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+                transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            }
         }
-        speed = Mathf.Clamp(currentSpeed, 0f, 10f);
-    }
+    
 }
     
 
