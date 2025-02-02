7d734bc14a9daefcc008b03dbd223927193c842f
MyPleasure <kir_zinn@mail.ru>
2025-02-02 22:15:16 +0300

    small fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,42 +6,46 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-        public float walkingSpeed = 3f;
-        public float runningSpeed = 6f;
-        public float jumpSpeed = 5f;
-        public float gravity = 20.0f;
-        public Camera playerCamera;
-        public float lookSpeed = 2.0f;
-        public float lookXLimit = 45.0f;
-        private QTEManager _qteManager;
-        private bool isQTEActive = false;
-        bool isLookingAtObject = false;
+    public float walkingSpeed = 3f;
+    public float runningSpeed = 6f;
+    public float jumpSpeed = 5f;
+    public float gravity = 20.0f;
+    public Camera playerCamera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+    private QTEManager _qteManager;
+    private bool isQTEActive = false;
+    bool isLookingAtObject = false;
 
-        public static int matches = 0;
-        public int Matches { get { return matches; } set {
-            matches = value;
-        }
+    public static int matches = 0;
+    public int Matches { 
+        get { return matches; } 
+        set { matches = value; }
     }
 
-        public CharacterController characterController;
-        Vector3 moveDirection = Vector3.zero;
-        float rotationX = 0;
+    public CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
 
-        [HideInInspector]
-        public bool canMove = true;
+    [HideInInspector]
+    public bool canMove = true;
 
 
     public static event Action<bool> isLookingAtInteractiveObj; // событие для подсказки
+    private LayerMask rayMask;
 
     void Start()
-        {
-            characterController = GetComponent<CharacterController>();
+    {
+        characterController = GetComponent<CharacterController>();
 
-            // Lock cursor
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-        void FindQTE() {
+        rayMask = LayerMask.GetMask("Interactable");
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void FindQTE() 
+    {
         _qteManager = GameObject.FindWithTag("QTEManager").GetComponent<QTEManager>();
         if (_qteManager != null)
         {
@@ -49,61 +53,61 @@ public class PlayerController : MonoBehaviour
             _qteManager.EndQTEEvent += StopQTE;
             Debug.Log("Событие нашлось");
         }
-        }
+    }
 
     void Update()
-        {
+    {
             
-            if (_qteManager == null)
-            {
-                FindQTE();
-                Debug.Log("Ничего нет");
-            }
-            if (isQTEActive)
-            {
-                return; // Игрок не может двигаться или вращать камеру
-            }
-            // We are grounded, so recalculate move direction based on axes
-            Vector3 forward = transform.TransformDirection(Vector3.forward);
-            Vector3 right = transform.TransformDirection(Vector3.right);
-            // Press Left Shift to run
-            bool isRunning = Input.GetKey(KeyCode.LeftShift);
-            float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-            float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
-            float movementDirectionY = moveDirection.y;
-            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        if (_qteManager == null)
+        {
+            FindQTE();
+            Debug.Log("Ничего нет");
+        }
+        if (isQTEActive)
+        {
+            return; // Игрок не может двигаться или вращать камеру
+        }
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-            if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-            {
-                moveDirection.y = jumpSpeed;
-            }
-            else
-            {
-                moveDirection.y = movementDirectionY;
-            }
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
 
-            // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-            // as an acceleration (ms^-2)
-            if (!characterController.isGrounded)
-            {
-                moveDirection.y -= gravity * Time.deltaTime;
-            }
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
 
-            // Move the controller
-            characterController.Move(moveDirection * Time.deltaTime);
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
 
-            // Player and Camera rotation
-            if (canMove)
-            {
-                rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-                rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-                playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-                transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-            }
+        // Player and Camera rotation
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
         RaycastingToObjects();
             
-        }
+    }
     void StartQTE()
     {
         isQTEActive = true;
@@ -123,8 +127,9 @@ public class PlayerController : MonoBehaviour
         TogglePointLight lightCol = null;
         MatchBox _matchBox = null;
         //Debug.Log(isLookingAtObject);
-        if (Physics.Raycast(ray, out hit, 0.6f))
+        if (Physics.Raycast(ray, out hit, 0.6f, rayMask))
         {
+            Debug.Log(hit.transform.name);
             //Debug.Log("Имя объекта: " + hit.transform.name);
             if (hit.transform.name == "Point Light")
             {
